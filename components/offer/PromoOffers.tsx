24b34271b7800ac1c0255e3497cc849e7ccbb872
mihad-khadem <mihadkhadem@gmail.com>
2026-01-14 21:14:37 +0600

    feat(offers):copy to clipboard

--- components/offer/PromoOffers.tsx
+++ components/offer/PromoOffers.tsx
@@ -1,5 +1,7 @@
-import { BadgePercent } from "lucide-react";
-import React from "react";
+"use client";
+import { BadgePercent, Copy, Check } from "lucide-react";
+import React, { useState } from "react";
+
 const promoCodes = [
   { code: "KACCHI20", desc: "20% OFF on orders above 800৳" },
   { code: "DINENOW", desc: "Flat 100৳ off" },
@@ -7,6 +9,14 @@ const promoCodes = [
 ];
 
 export const PromoOffers = () => {
+  const [copied, setCopied] = useState(null);
+
+  const handleCopy = async (code: any) => {
+    await navigator.clipboard.writeText(code);
+    setCopied(code);
+    setTimeout(() => setCopied(null), 1500);
+  };
+
   return (
     <>
       <div className="bg-white dark:bg-neutral-800 rounded-xl p-6 shadow">
@@ -21,9 +31,21 @@ export const PromoOffers = () => {
           {promoCodes.map((promo, index) => (
             <div
               key={index}
-              className="border border-dashed border-yellow-500 rounded-lg p-4 flex flex-col items-center text-center"
+              onClick={() => handleCopy(promo.code)}
+              className="border border-dashed border-yellow-500 rounded-lg p-4 flex flex-col items-center text-center cursor-pointer hover:bg-yellow-50 dark:hover:bg-neutral-700 transition"
             >
-              <p className="text-lg font-bold text-yellow-400">{promo.code}</p>
+              <div className="flex items-center gap-2">
+                <p className="text-lg font-bold text-yellow-400">
+                  {promo.code}
+                </p>
+
+                {copied === promo.code ? (
+                  <Check className="w-4 h-4 text-green-500" />
+                ) : (
+                  <Copy className="w-4 h-4 text-yellow-400 opacity-70" />
+                )}
+              </div>
+
               <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-1">
                 {promo.desc}
               </p>
